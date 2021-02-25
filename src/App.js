a95007aeb68d30cb6b29508a37ab7de87606a18a
codewithdpk <codewithdeepak@yahoo.com>
2021-02-25 15:04:54 +0530

    Intial code.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Card, Col, Row, Button } from "react-bootstrap";
 
-function App() {
+const App = () => {
+  // Store all products
+  const [Products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Gettting products data from dummy api
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        //setProducts(json);
+        console.log(json);
+        const n = 3;
+        const result = new Array(Math.ceil(json.length / n))
+          .fill()
+          .map((_) => json.splice(0, n));
+        setProducts(result);
+        console.log(result);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col lg={12}>
+        {Products.map((product) => {
+          return (
+            <Row className="justify-content-lg-center">
+              {product.map((item) => {
+                return (
+                  <Col lg={4} md={6} sm={6}>
+                    <Card style={{ margin: "20px" }}>
+                      <Card.Img
+                        variant="top"
+                        src={item.image}
+                        style={{ height: "150px" }}
+                      />
+                      <Card.Body>
+                        <Card.Title>{item.title}</Card.Title>
+                        <Card.Text
+                          style={{
+                            textOverflow: "ellipsis",
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                          }}
+                        >
+                          {item.description}
+                        </Card.Text>
+                        <Button variant="primary">Add To Cart</Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          );
+        })}
+      </Col>
+    </Row>
   );
-}
+};
 
 export default App;
